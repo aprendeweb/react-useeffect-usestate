7b02a3d1540fa93c5cfddfa078c0b195392324ac
David Uribe <david.uribes@perficient.com>
2021-09-06 22:14:43 -0500

    React Use State and Use Effect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [update, setUpdate] = useState(false);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => { setTasks(json) })
+  }, []);
+
+  useEffect(() => {
+    console.log("Me ejecuté")
+  }, [update]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section>
+        <button onClick={() => setUpdate(!update)}>Update</button>
+      </section>
+      <section>
+        {tasks.length ?
+          tasks.map((task) => {
+            return (
+              <span key={task.id}>
+                <p>{task.title}</p>
+              </span>)
+          })
+          : null}
+      </section>
     </div>
-  );
+  )
 }
 
 export default App;
